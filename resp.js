b1f06687d48695fa57cd91cd6c64bfa9fecbba02
Tanish sahu <68512022+tanish96@users.noreply.github.com>
2021-02-28 23:56:41 +0530

    spire consultancy services

---
+++ resp.js
@@ -0,0 +1,42 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navList=document.querySelector('.nav-list')
+rightNav=document.querySelector('.rightNav')
+burger .addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+});
+
+
+function start(){
+    document.getElementById("mytuto").classList.toggle("show");
+}
+window.onclick=function(event){
+    if (!event.target.matches('.bt')) {
+        var dropdowns = document.getElementsByClassName("tuto-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+}
+function st(){
+  document.getElementById("mystu").classList.toggle("show");
+}
+window.onclick=function(event){
+  if (!event.target.matches('.st')) {
+      var dropdowns = document.getElementsByClassName("stu-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+        
+      }
+    }
+}
